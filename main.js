549ceab8396a71fdc3c2796561125444a712da20
Tammy Tian <tiantammy@outlook.com>
2018-04-03 05:01:37 +0800

    added rps-game function

--- main.js
+++ main.js
@@ -25,66 +25,85 @@
 
 // console.log( beyond(3) );
 
-const decode = function (secretWord) {
-    if (secretWord[0] === 'a') {
-        return secretWord[1];
-    } else if (secretWord[0] === 'b') {
-        return secretWord[2];
-    } else if (secretWord[0] === 'c') {
-        return secretWord[3];
-    } else if (secretWord[0] === 'd') {
-        return secretWord[4];
-    } else {
-        return " ";
+// const decode = function (secretWord) {
+//     if (secretWord[0] === 'a') {
+//         return secretWord[1];
+//     } else if (secretWord[0] === 'b') {
+//         return secretWord[2];
+//     } else if (secretWord[0] === 'c') {
+//         return secretWord[3];
+//     } else if (secretWord[0] === 'd') {
+//         return secretWord[4];
+//     } else {
+//         return " ";
+//     }
+// }
+// console.log(decode('craft'));
+
+// console.log(decode('block'));
+
+// console.log(decode('argon'));
+
+// console.log(decode('meter'));
+
+// console.log(decode('bells'));
+
+// console.log(decode('brown'));
+
+// console.log(decode('croon'));
+
+// console.log(decode('droop'));
+
+// const daysInMonth = (month, leapYear) => {
+
+// 	let result = '';
+
+// 	switch(month) {
+// 		case 'January':
+// 		case 'March':
+// 		case 'May':
+// 		case 'July':
+// 		case 'August':
+// 		case 'October':
+// 		case 'December':
+// 			result = `${month} has 31 days.`;
+// 			break;
+// 		case 'February':
+// 			if (leapYear) {
+// 				result = `${month} has 29 days.`;
+// 			} else {
+// 					result = `${month} has 28 days.`;
+// 			}
+// 		case 'April':
+// 		case 'June':
+// 		case 'September':
+// 		case 'November':
+// 			result = `${month} has 30 days.`;
+// 			break;
+// 		default: 
+// 			result = 'Must provide a valid month';
+// 	}
+
+// 	return result;
+// }
+
+// daysInMonth('March');
+
+
+
+const rpsGame = function (player1, player2) {
+    const randomNo = Math.floor(Math.random() * 3) + 1;
+   
+    if (player1 === player2) {
+        return "It's a tie.";
+    } else if (player1 > player2) {
+        return "Player 1 Wins";
+    } else if (player1 < player2) {
+        return "Player 2 Wins."
+    }
+
+    if (typeof player1 !== 'number') {
+        throw new Error ('Please put in a number to represent rock, paper or scissor');
     }
 }
-console.log(decode('craft'));
-
-console.log(decode('block'));
-
-console.log(decode('argon'));
-
-console.log(decode('meter'));
-
-console.log(decode('bells'));
-
-console.log(decode('brown'));
-
-console.log(decode('croon'));
-
-console.log(decode('droop'));
-
-const daysInMonth = (month, leapYear) => {
-
-	let result = '';
-
-	switch(month) {
-		case 'January':
-		case 'March':
-		case 'May':
-		case 'July':
-		case 'August':
-		case 'October':
-		case 'December':
-			result = `${month} has 31 days.`;
-			break;
-		case 'February':
-			if (leapYear) {
-				result = `${month} has 29 days.`;
-			} else {
-					result = `${month} has 28 days.`;
-			}
-		case 'April':
-		case 'June':
-		case 'September':
-		case 'November':
-			result = `${month} has 30 days.`;
-			break;
-		default: 
-			result = 'Must provide a valid month';
-	}
-
-	return result;
-}
-
-daysInMonth('March');
+console.log (rpsGame('q',2));
